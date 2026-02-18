0b0d5747e12b34edcea30c6a3500788434a6f323
mirbekovx <mirbekovx21@gmail.com>
2026-02-18 15:54:13 +0000

    save server changes

--- pet_project0/blog/views.py
+++ pet_project0/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post # если вы выводите посты
 
-# Create your views here.
+def post_list(request):
+    # Достаем все посты из базы данных
+    posts = Post.objects.all()
+    # Передаем их в шаблон под именем 'posts'
+    return render(request, 'blog/post_list.html', {'posts': posts})
